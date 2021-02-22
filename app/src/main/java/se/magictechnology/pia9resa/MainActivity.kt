3bc1f557d9136c7fdc8098fc0716ac29c31e38a5
Bill Mårtensson <bill@magictechnology.se>
2021-02-22 11:32:00 +0100

    Travel

--- app/src/main/java/se/magictechnology/pia9resa/MainActivity.kt
+++ app/src/main/java/se/magictechnology/pia9resa/MainActivity.kt
@@ -15,8 +15,15 @@ import java.net.URL
 
 data class ApiData(val StopLocation: List<StopLoc>)
 
-data class StopLoc(val name: String)
+data class StopLoc(val id : String, val name: String)
 
+
+
+data class TravelInfo(val Trip : List<Tripinfo>)
+data class Tripinfo(val LegList : LegListInfo)
+data class LegListInfo(val Leg : List<LegInfo>)
+data class LegInfo(val Origin : StopTravelInfo, val Destination : StopTravelInfo)
+data class StopTravelInfo(val name : String, val time : String)
 
 
 class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
@@ -33,15 +40,27 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
             val toEditText = findViewById<EditText>(R.id.toEditText)
 
             launch {
-                loadapi(fromEditText.text.toString())
+                Log.d("pia9debug", "Starta hämtning")
+
+                val fromStop = loadapi(fromEditText.text.toString())
+                Log.d("pia9debug", fromStop.name)
+
+                val toStop = loadapi(toEditText.text.toString())
+                Log.d("pia9debug", toStop.name)
+
+                Log.d("pia9debug", "Hämtning klar")
+
+                findTrip(fromStop, toStop)
+
+
             }
         }
 
     }
 
-    suspend fun loadapi(searchstring : String)
+    suspend fun loadapi(searchstring : String) : StopLoc
     {
-        withContext(Dispatchers.IO) {
+        return withContext(Dispatchers.IO) {
             val theurl = URL("https://api.resrobot.se/location.name.json?key=47898ae7-fbde-4641-a2bd-d5842a0eb67d&input="+searchstring)
             /*
             var resultString = theurl.readText()
@@ -60,6 +79,8 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
             val theStops = Gson().fromJson(theResultString, ApiData::class.java)
 
+            return@withContext theStops.StopLocation[0]
+            /*
             withContext(Dispatchers.Main) {
                 Log.d("pia9debug", "RESULTAT")
                 Log.d("pia9debug", theResultString)
@@ -67,14 +88,43 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
                 for(somestop in theStops.StopLocation)
                 {
                     Log.d("pia9debug", somestop.name)
-                    findViewById<TextView>(R.id.resultTextView).text = somestop.name
+                    findViewById<TextView>(R.id.resultTextView).text = somestop.id
                 }
             }
-
+            */
 
         }
 
     }
 
+
+    suspend fun findTrip(fromStop : StopLoc, toStop : StopLoc)
+    {
+        Log.d("pia9debug", "Nu hitta resa")
+
+        withContext(Dispatchers.IO) {
+
+            val theurl = URL("https://api.resrobot.se/v2/trip?key=47898ae7-fbde-4641-a2bd-d5842a0eb67d&originId=" + fromStop.id + "&destId="+ toStop.id +"&format=json")
+
+            Log.d("pia9debug", theurl.toString())
+
+            val theConnection = (theurl.openConnection() as? HttpURLConnection)!!.apply {
+                requestMethod = "GET"
+                setRequestProperty("Content-Type", "application/json; charset=utf-8")
+                setRequestProperty("Accept", "application/json")
+            }
+
+            val reader = BufferedReader(theConnection.inputStream.reader())
+
+            val theResultString = reader.readText()
+
+            val thetravel = Gson().fromJson(theResultString, TravelInfo::class.java)
+
+            Log.d("pia9debug", thetravel.Trip[0].LegList.Leg[0].Origin.time)
+            Log.d("pia9debug", thetravel.Trip[0].LegList.Leg[0].Destination.time)
+        }
+
+
+    }
 
 }
